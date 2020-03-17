db6350e42f2da2b631bd1b9576c805c262f6fc55
wakkarkhan <muhammadwakkar777@gmail.com>
2020-03-17 11:44:32 +0500

    17march , second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Lets-Go
                 </div>
 
                 <div class="links">
